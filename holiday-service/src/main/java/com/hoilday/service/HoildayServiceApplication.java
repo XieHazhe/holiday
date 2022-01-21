5762017b3f515815751716a7c214a9a2be3d99bb
xiehaozhe <wb-xhz775545@alibaba-inc.com>
2022-01-21 17:53:31 +0800

    init

---
+++ holiday-service/src/main/java/com/hoilday/service/HoildayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hoilday.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoildayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoildayServiceApplication.class, args);
+    }
+
+}
---
+++ holiday-service/src/test/java/com/hoilday/service/HoildayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoilday.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoildayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
